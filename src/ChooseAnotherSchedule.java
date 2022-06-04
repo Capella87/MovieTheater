8bf8e7fc9f34c817407806aa87878ffc982c1a53
Capella87 <contact@capelladev.com>
2022-06-04 12:19:20 +0900

    예약 변경 버튼 이벤트 부분 구현

--- src/ChooseAnotherSchedule.java
+++ src/ChooseAnotherSchedule.java
@@ -1,7 +1,10 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -14,6 +17,8 @@ import java.sql.ResultSetMetaData;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class ChooseAnotherSchedule extends JDialog
 {
@@ -21,29 +26,35 @@ public class ChooseAnotherSchedule extends JDialog
     private final JPanel contentPanel = new JPanel();
     private Database db;
     private Object[] bookingInfo;
+    private ArrayList<Object[]> altScheduleInfo;
     private int origSchId;
     private int movieId;
     private JTable resultTable;
     final int saleprice = 10000;
+    private TableResult wrapperModel = null;
     private JScrollPane scrollPane;
 
     /**
      * Create the dialog.
      */
-    public ChooseAnotherSchedule(Database db, int movieId, Object[] bookingInfo, int origScheduleId)
+    public ChooseAnotherSchedule(Database db, Object[] bookingInfo)
     {
         this.db = db;
         this.bookingInfo = bookingInfo;
-        this.movieId = movieId;
-        origSchId = origScheduleId; // 원래 스케줄
+        this.movieId = (Integer)bookingInfo[0];
+        origSchId = (Integer)bookingInfo[4]; // 원래 스케줄
         setTitle("예매 일정 변경");
         setBounds(100, 100, 450, 300);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new BorderLayout());
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
         
+        scrollPane = new JScrollPane();
+        altScheduleInfo = new ArrayList<Object[]>();
+        
         try {
-            var getAltSchedule = new StringBuilder("SELECT theater_id, date FROM schedules "
+            var getAltSchedule = new StringBuilder("SELECT theater_id, date, start_time, schedule_id FROM schedules "
                     + "WHERE schedule_id != ");
             getAltSchedule.append(origSchId); // 주의
             getAltSchedule.append(" and movie_id = ");
@@ -59,13 +70,13 @@ public class ChooseAnotherSchedule extends JDialog
 
             if (getSchedules == null || altScheduleCount == 0) {
                 JOptionPane.showMessageDialog(null, "해당 영화가 없습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
-                 // ta.append("\n해당 영화가 없습니다. \n\n");
+                dispose(); // 화면 닫기
             } else {
                 ResultSetMetaData columns = getSchedules.getMetaData();
                 int columnCount = columns.getColumnCount();
 
                 var resultModel = new DefaultTableModel(altScheduleCount, 0);
-                for (int i = 2; i <= columnCount; i++) {
+                for (int i = 2; i <= columnCount - 1; i++) {
                         resultModel.addColumn(columns.getColumnName(i));
                 }
                             
@@ -74,9 +85,10 @@ public class ChooseAnotherSchedule extends JDialog
                             
                 for (int i = 0; i < altScheduleCount; i++) {
                             
-                    for (int j = 2; j <= columnCount; j++) {
+                    for (int j = 2; j <= columnCount - 1; j++) {
                          resultModel.setValueAt(getSchedules.getString(j), i, j - 2); // 주의
                     }
+                    this.altScheduleInfo.add(new Object[] { getSchedules.getInt(columnCount) }); // schedule_id 삽입
                     getSchedules.next();
                  }
                             
@@ -89,7 +101,7 @@ public class ChooseAnotherSchedule extends JDialog
             
         }
         catch (SQLException e) {
-            
+            e.printStackTrace();
         }
        
         
@@ -97,8 +109,12 @@ public class ChooseAnotherSchedule extends JDialog
         
         contentPanel.setLayout(new BorderLayout(0, 0));
         {
-            JScrollPane scrollPane = new JScrollPane();
             contentPanel.add(scrollPane, BorderLayout.CENTER);
+        }
+        {
+            JLabel lblNewLabel = new JLabel("다른 일정을 선택하세요.");
+            lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            contentPanel.add(lblNewLabel, BorderLayout.NORTH);
         }
         {
             JPanel buttonPane = new JPanel();
@@ -106,13 +122,63 @@ public class ChooseAnotherSchedule extends JDialog
             getContentPane().add(buttonPane, BorderLayout.SOUTH);
             {
                 JButton changeScheduleButton = new JButton("일정 변경");
-                changeScheduleButton.setActionCommand("OK");
+                changeScheduleButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        if (wrapperModel == null || resultTable == null) return;
+                        
+                        try {
+                            int rowCount = resultTable.getRowCount();
+                            int lastSelectedIdx = -1;
+                            int selectedCount = 0;
+                            
+                            for (int i = 0; i < rowCount; i++) {
+                                Boolean isSelected = (Boolean)resultTable.getValueAt(i, 0);
+                                
+                                if (isSelected && selectedCount == 0) {
+                                    lastSelectedIdx = i;
+                                    selectedCount++;
+                                }
+                                else if (isSelected && selectedCount > 0) {
+                                    throw new MultipleSelectedException("한 개만 선택할 수 있습니다.");
+                                }
+                            }
+                            if (lastSelectedIdx == -1) {
+                                JOptionPane.showMessageDialog(null, "영화를 선택하십시오.", "ERROR", JOptionPane.ERROR_MESSAGE);
+                                return;
+                            }
+                            else {
+                                try {
+                                    // 예매 변경
+                                    
+                                    
+                                    // var choose = new ChooseAnotherSchedule(db, altScheduleInfo.get(lastSelectedIdx)[0]);
+                                    // choose.setModalityType(JDialog.ModalityType.APPLICATION_MODAL);
+                                    // choose.setVisible(true);
+                                }
+                                catch (Exception err) {
+                                    JOptionPane.showMessageDialog(null, "예매 변경 실패", "ERROR",
+                                            JOptionPane.ERROR_MESSAGE);
+                                    err.printStackTrace();
+                                }
+                            }
+                        }
+                            catch (MultipleSelectedException e) {
+                                JOptionPane.showMessageDialog(null, err.getMessage(), "ERROR",
+                                        JOptionPane.ERROR_MESSAGE);
+                                err.printStackTrace();
+                            }
+                    }
+                });
                 buttonPane.add(changeScheduleButton);
                 getRootPane().setDefaultButton(changeScheduleButton);
             }
             {
                 JButton cancelButton = new JButton("취소");
-                cancelButton.setActionCommand("Cancel");
+                cancelButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        dispose();
+                    }
+                });
                 buttonPane.add(cancelButton);
             }
         }
